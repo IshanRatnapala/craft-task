89370717bd78791a56ec19399261bf334c0b0674
Ishan Ratnapala <ishanratnapala@gmail.com>
2026-06-01 11:37:31 +0930

    Fix JSON import

--- src/api/employeeData.ts
+++ src/api/employeeData.ts
@@ -1,7 +1,20 @@
-export const getEmployees = async () => {
+import applicants from '../data/applicants.json';
+import type { Employee } from '../types';
+
+export const getEmployees = async (): Promise<Employee[]> => {
     // delay for debugging
     await new Promise(resolve => setTimeout(resolve, 2000));
-    const response = await fetch('/data/applicants.json');
-    const data = await response.json();
-    return data;
+    return applicants.map((employee) => ({
+        ...employee,
+        status: employee.status as Employee['status'],
+    }));
+}
+
+export const getDepartments = async () => {
+    const employees = await getEmployees();
+    const departments = new Set<string>();
+    employees.forEach((employee: { department: string }) => {
+        departments.add(employee.department);
+    });
+    return Array.from(departments);
 }
